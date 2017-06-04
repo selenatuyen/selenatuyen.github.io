7de5bac027b5da3405918610d00caae04aeba93f
Selena Tuyen <sltuyen@ucdavis.edu>
2017-06-04 16:15:54 -0700

    Navigation arrows fully working

--- js/glossary.js
+++ js/glossary.js
@@ -1,21 +1,29 @@
 $("#nextButt").click(function(){
 	var id = $(".active").attr("id");
 	var num = id.charAt(1);
+	if (num == 4){
+		$("#nextButt").addClass("hidden");
+	}
 	$("#"+id).removeClass("active");
-	if(num < 6){
+	if(num < 5){
 		var newId = "#p" + (parseInt(num) + 1);
-		console.log(newId);		
 		$(newId).addClass("active");
+		$("#prevButt").removeClass("hidden");
 	}
 });
 
 $("#prevButt").click(function(){
 	var id = $(".active").attr("id");
 	var num = id.charAt(1);
+	if((num-1) == 0){
+		$("#prevButt").addClass("hidden");
+	}
+	if(num == 5){
+		$("#nextButt").removeClass("hidden");
+	}
 	$("#"+id).removeClass("active");
-	if(num > 0 && num < 6){
+	if((num > 0) && (num < 6)){
 		var newId = "#p" + (parseInt(num) - 1);
-		console.log(newId);		
 		$(newId).addClass("active");
 	}
 });
