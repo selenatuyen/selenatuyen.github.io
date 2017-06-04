552eceb31ca2e23b8bed334eb5c994c7983d8fad
Selena Tuyen <sltuyen@ucdavis.edu>
2017-06-04 15:42:22 -0700

    Add pagination for glossary

--- js/glossary.js
+++ js/glossary.js
@@ -1,3 +1,21 @@
-$("#nextButt".click(function(){
-	$("#p1").hide(500).css({visibility: "hidden", display: ""});
-}))
+$("#nextButt").click(function(){
+	var id = $(".active").attr("id");
+	var num = id.charAt(1);
+	$("#"+id).removeClass("active");
+	if(num < 6){
+		var newId = "#p" + (parseInt(num) + 1);
+		console.log(newId);		
+		$(newId).addClass("active");
+	}
+});
+
+$("#prevButt").click(function(){
+	var id = $(".active").attr("id");
+	var num = id.charAt(1);
+	$("#"+id).removeClass("active");
+	if(num > 0 && num < 6){
+		var newId = "#p" + (parseInt(num) - 1);
+		console.log(newId);		
+		$(newId).addClass("active");
+	}
+});
